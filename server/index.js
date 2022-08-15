f9fc9232cdfd5e55853ca751c72ec6a4dcd23c70
maddybaum <madeline.baum94@gmail.com>
2022-08-15 14:10:24 -0500

    initial commit for Aug 15 lab

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+})
+
+app.get("/weather/:temperature", (req, res) => {
+    const {temperature} = req.params;
+    const phrase = `<h3>It was ${temperature} today </h3>`;
+    res.status(200).send(phrase);
+})
+const SERVER_PORT = 4000;
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on ${SERVER_PORT}`)
+})
